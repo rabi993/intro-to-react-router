b6d8523eae19f32fe5818ebfd53439a73dc32d13
Rabiul Islam <52354711+rabi993@users.noreply.github.com>
2023-10-14 23:22:53 +0600

    54-6 (Recap) Dynamic Data loader and navigate on button click

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import "./Post.css";
 
 const Post = ({post}) => {
     const { id, title, body} = post;
+    const navigate = useNavigate();
+
+    const handleNavigation = () =>{
+        navigate(`/post/${id}`);
+    }
+
     return (
         <div className='post'>
             <h5>ID:{id}</h5>
             <h5>{title}</h5>
             <p>{body}</p>
             <Link to={`/post/${id}`}>Show Details</Link>
-            <button>Show post Details</button>
+            <Link to={`/post/${id}`}><button>Show post Details</button></Link>
+            <button onClick={handleNavigation}>With button Handler</button>
+            
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -14,6 +14,7 @@ import First from './components/First/First.jsx';
 import Friends from './components/Friends/Friends.jsx';
 import FriendDetail from './components/FriendDetail/FriendDetail.jsx';
 import Posts from './components/Posts/Posts.jsx';
+import PostDetail from './components/PostDetail/PostDetail.jsx';
 
 // const router =createBrowserRouter([
 //   {
@@ -54,6 +55,11 @@ const router = createBrowserRouter([
         element: <Posts></Posts>,
         loader: () => fetch('https://jsonplaceholder.typicode.com/posts')
 
+      },
+      {
+        path:"post/:postId",
+        element: <PostDetail></PostDetail>,
+        loader: ({params}) => fetch(`https://jsonplaceholder.typicode.com/posts/${params.postId}`)
       },
       {
         path: "about",
