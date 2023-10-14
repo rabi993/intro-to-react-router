87638ff3a0328fcee09ef3a2b027067dc6954424
Rabiul Islam <52354711+rabi993@users.noreply.github.com>
2023-10-14 23:51:47 +0600

    54-7 Active route, Loading Spinner and Not Found

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import "./Header.css";
+import ActiveLink from '../ActiveLink/ActiveLink';
 
 const Header = () => {
     return (
@@ -8,11 +9,18 @@ const Header = () => {
             {/* <a href="/">Home</a>
             <a href="/about">About</a>
             <a href="/contact">Contact</a> */}
-            <Link to="/" >Home</Link>
-            <Link to="/friends">Friends</Link>
+
+
+            {/* <Link to="/" >Home</Link>
+            <ActiveLink to="/friends">Friends</ActiveLink>
             <Link to="/posts">Posts</Link>
             <Link to="/about">About</Link>
-            <Link to="/contact">Contact</Link>
+            <Link to="/contact">Contact</Link> */}
+            <ActiveLink to="/" >Home</ActiveLink>
+            <ActiveLink to="/friends">Friends</ActiveLink>
+            <ActiveLink to="/posts">Posts</ActiveLink>
+            <ActiveLink to="/about">About</ActiveLink>
+            <ActiveLink to="/contact">Contact</ActiveLink>
         </nav>
     );
 };
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigation } from 'react-router-dom';
 import Header from '../Header/Header';
 
 const Home = () => {
+    const navigation = useNavigation();
+
     return (
         <div>
             <Header></Header>
-            <h3>This is Home</h3>
+            {/* <h3>This is Home</h3> */}
+            {/* <div>{navigation.state === 'loading' ? 'Loading...' : '' }</div> */}
+            <div>{navigation.state === 'loading' && 'Loading...'}</div> 
             <Outlet></Outlet>
         </div>
     );
