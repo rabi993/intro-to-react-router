efa52e7b186eeab1f50f6c651b7e56c1289806a9
Rabiul Islam <52354711+rabi993@users.noreply.github.com>
2023-10-14 20:40:47 +0600

    create element and children in router class

--- src/main.jsx
+++ src/main.jsx
@@ -6,24 +6,47 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import About from './components/About/About.jsx';
+import Contact from './components/Contact/Contact.jsx';
+import Header from './components/Header/Header.jsx';
+import Home from './components/Home/Home.jsx';
 
-const router =createBrowserRouter([
+// const router =createBrowserRouter([
+//   {
+//   path:"/",
+//   element: <App></App>
+//   },
+//   {
+//     path: "/about",
+//     element: <About></About>
+//   },
+//   {
+//     path: "/contact",
+//     element: <Contact></Contact>
+//   }
+// ])
+
+const router = createBrowserRouter([
   {
-  path:"/",
-  element: <App></App>
-  },
-  {
-    path: "/about",
-    element: <div>This is the about page</div>
-  },
-  {
-    path: "/contact",
-    element: <div>This is the contact page</div>
+    path:"/",
+    element:<Home></Home>,
+    children:[
+      {
+        path: "about",
+        element:<About></About>
+      },
+      {
+        path: "contact",
+        element:<Contact></Contact>
+      }
+    ]
   }
 ])
 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    {/* <Header></Header> */}
     <RouterProvider router={router} ></RouterProvider>
   </React.StrictMode>,
 )
